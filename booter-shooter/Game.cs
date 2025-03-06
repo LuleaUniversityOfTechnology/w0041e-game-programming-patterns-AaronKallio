e015548e92c131014df016476dcfc65845224283
AaronKallio <aaron.j.kallio@gmail.com>
2025-03-06 18:17:08 +0100

    fairies can be shot

--- booter-shooter/Game.cs
+++ booter-shooter/Game.cs
@@ -3,14 +3,16 @@ using System;
 
 public partial class Game : Node
 {
-    /*
+    
     [Export]
     public PackedScene bird_manager { get; set; }
+
+   
     public override void _Ready(){
-        fairyNode fairy1 = bird_manager.fairyReady<fairyNode>();
-        addChild(fairy1);
+        //fairyNode fairy1 = bird_manager.fairyReady<fairyNode>();
+        //addChild(fairy1);
     }
-*/
+
 
     
 }
